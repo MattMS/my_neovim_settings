d86802090e603f8a88e6d5838295afb11af63bf0
Matt McKellar-Spence <matt+git@mattms.com>
2022-08-17 09:57:52 +1000

    Fix adding new lines around selection

--- lua/add.lua
+++ lua/add.lua
@@ -1,17 +1,28 @@
+-- Uses `v` along with `.` so it works in Visual and other modes.
+--
+-- Using `current_line_number()` will fail when it is in a different window (like fzf)
+
 M = {}
 
 M.lines = {}
 
+-- TODO: Fix when `v` is below `.`
+local function get_start_line ()
+	return math.min(vim.fn.line('.'), vim.fn.line('v') - 1)
+end
+
+local function get_stop_line ()
+	return math.max(vim.fn.line('.'), vim.fn.line('v') - 1)
+end
+
 -- Adds the given text above the current line.
 M.lines.above = function (text)
-	-- Uses `v` instead of `.` so it works in Visual and other modes.
-	vim.fn.append(vim.fn.line('v') - 1, text)
+	vim.fn.append(get_start_line(), text)
 end
 
 -- Adds the given text below the current line.
 M.lines.below = function (text)
-	-- Using `current_line_number()` will fail when it is in a different window (like fzf)
-	vim.fn.append(vim.fn.line('.'), text)
+	vim.fn.append(get_stop_line(), text)
 end
 
 M.prefix = function (prefix, lines)
